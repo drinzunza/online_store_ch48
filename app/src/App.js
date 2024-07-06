db38013f5998c2b4fed7d70a3c0f4e6a16722478
Sergio Inzunza <inxunxa@gmail.com>
2024-07-06 11:27:24 -0700

    Notest

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Catalog from './pages/catalog';
 import About from './pages/about';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
+import './App.css';
 function App() {
   return (
-    <div className="App">      
+    <div className="App">
       <Navbar />
 
       <Catalog />
@@ -20,4 +20,5 @@ function App() {
   );
 }
 
+// this is a test change
 export default App;
